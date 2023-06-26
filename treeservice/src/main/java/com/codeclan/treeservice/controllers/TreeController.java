9dc78c6496a382c527315f738fc2849d7e592bee
zoe <zoeeustace@hotmail.com>
2023-06-26 13:36:50 +0100

    get all trees and get tree by ID mapping set up

--- treeservice/src/main/java/com/codeclan/treeservice/controllers/TreeController.java
+++ treeservice/src/main/java/com/codeclan/treeservice/controllers/TreeController.java
@@ -1,7 +1,30 @@
 package com.codeclan.treeservice.controllers;
 
+import com.codeclan.treeservice.models.Tree;
+import com.codeclan.treeservice.repositories.TreeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class TreeController {
+
+    @Autowired
+    TreeRepository treeRepository;
+
+    @GetMapping(value="/trees")
+    public ResponseEntity<List<Tree>> getAllTrees(){
+        return new ResponseEntity<>(treeRepository.findAll(), HttpStatus.OK);
+    }
+
+    @GetMapping(value="/trees/{id}")
+    public ResponseEntity<Optional<Tree>> getTree(@PathVariable Long id){
+        return new ResponseEntity<>(treeRepository.findById(id), HttpStatus.OK);
+    }
 }
