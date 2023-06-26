57f4cd1d80431d8ea8fb763263f4bd6a1504bb64
zoe <zoeeustace@hotmail.com>
2023-06-26 13:45:36 +0100

    get mapping and post mapping set up for user

--- treeservice/src/main/java/com/codeclan/treeservice/controllers/UserController.java
+++ treeservice/src/main/java/com/codeclan/treeservice/controllers/UserController.java
@@ -1,7 +1,34 @@
 package com.codeclan.treeservice.controllers;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.codeclan.treeservice.models.User;
+import com.codeclan.treeservice.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class UserController {
+
+    @Autowired
+    UserRepository userRepository;
+
+    @GetMapping(value="/users")
+    public ResponseEntity<List<User>> getAllUsers(){
+        return new ResponseEntity<>(userRepository.findAll(), HttpStatus.OK);
+    }
+
+    @GetMapping(value="/users/{id}")
+    public ResponseEntity<Optional<User>> getUserById(@PathVariable Long id){
+        return new ResponseEntity<>(userRepository.findById(id), HttpStatus.OK);
+    }
+
+    @PostMapping(value="/users")
+    public ResponseEntity<User> createUser(@RequestBody User user){
+        userRepository.save(user);
+        return new ResponseEntity<>(user, HttpStatus.CREATED);
+    }
 }
