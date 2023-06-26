46cb9af5cf12af92caee4f2db9c6b908e9db9459
zoe <zoeeustace@hotmail.com>
2023-06-26 14:15:38 +0100

    post request added for tree

--- treeservice/src/main/java/com/codeclan/treeservice/controllers/TreeController.java
+++ treeservice/src/main/java/com/codeclan/treeservice/controllers/TreeController.java
@@ -5,10 +5,9 @@ import com.codeclan.treeservice.repositories.TreeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import javax.xml.ws.Response;
 import java.util.List;
 import java.util.Optional;
 
@@ -26,5 +25,11 @@ public class TreeController {
     @GetMapping(value="/trees/{id}")
     public ResponseEntity<Optional<Tree>> getTree(@PathVariable Long id){
         return new ResponseEntity<>(treeRepository.findById(id), HttpStatus.OK);
+    }
+
+    @PostMapping(value="/trees")
+    public ResponseEntity<Tree> createTree(@RequestBody Tree tree){
+        treeRepository.save(tree);
+        return new ResponseEntity<>(tree, HttpStatus.CREATED);
     }
 }
