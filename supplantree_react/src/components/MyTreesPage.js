5ee0fa33101659071aad83a96363fabad7e71796
Richard Dalrymple <117475164+Tcherin@users.noreply.github.com>
2023-07-01 10:14:51 +0100

    styled my trees page

--- supplantree_react/src/components/MyTreesPage.js
+++ supplantree_react/src/components/MyTreesPage.js
@@ -1,20 +1,25 @@
-import React from 'react';
-import Request from '../helpers/request';
-import { useEffect, useState } from 'react';
+import React from "react";
+import Request from "../helpers/request";
+import { useEffect, useState } from "react";
 
-const MyTrees = ({users}) => {
+const MyTrees = ({ users }) => {
+  const usersTrees = users[0]["trees"].map((tree, index) => {
+    return (
+      <div className="bg-pink-200 border-4 border-white basis-1/4 text-center rounded-xl p-4 gap-6">
+        <p>{tree.species}</p>
+        <p>{tree.nature}</p>
+      </div>
+    );
+  });
 
-    const usersTrees = users[0]["trees"].map((tree, index) => {
-        return(
-            <p>{tree.species}</p>
-        )
-    })
+  return (
+    <div className="bg-green-400 min-h-screen">
+      <h1 className="text-6xl p-4">Your Digital Forest</h1>
+      <div className="flex flex-col md:flex-row flex-wrap gap-10 justify-evenly p-8">
+        {usersTrees}
+      </div>
+    </div>
+  );
+};
 
-    return ( 
-        <div>
-            {usersTrees}
-        </div>
-     );
-}
- 
 export default MyTrees;
