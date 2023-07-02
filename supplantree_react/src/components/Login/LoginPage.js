db7c9afac2fcffe9c6ae2986e9b8492683a4811a
Richard Dalrymple <117475164+Tcherin@users.noreply.github.com>
2023-07-02 12:30:27 +0100

    styled login and create account pages

--- supplantree_react/src/components/Login/LoginPage.js
+++ supplantree_react/src/components/Login/LoginPage.js
@@ -2,13 +2,29 @@ import React from "react";
 
 const Login = () => {
   return (
-    <div>
-      <div className="flex justify-center my-20">
-        <a href="http://localhost:3000/create-account">
-          <button className="ml-10 bg-slate-200 hover:bg-slate-400 transition duration-150 ease-out hover:ease-in hover:scale-110 m-4 p-3 w-36 rounded-lg shadow-lg shadow-gray-600">
-            Create Account
-          </button>
-        </a>
+    <div className="bg-green-400 min-h-screen">
+      <h1 className="text-6xl p-4">Login</h1>
+      <div className="flex justify-center">
+        <div className="flex flex-col justify-center w-1/3">
+          <div className="flex flex-col justify-center">
+            <form className="flex flex-col justify-center bg-pink-200 border-4 border-white rounded-2xl p-10">
+              <input className="p-1" type="text" placeholder="Username" />
+              <br />
+              <input className="p-1" type="password" placeholder="Password" />
+              <br />
+              <button className="place-self-center bg-slate-300 border-white border-4 hover:bg-pink-200 transition duration-150 ease-out hover:ease-in hover:scale-110 m-4 p-3 w-36 rounded-lg shadow-lg">
+                Login
+              </button>
+            </form>
+            <div className="flex justify-center">
+              <a href="http://localhost:3000/create-account">
+                <button className="bg-slate-300 border-white border-4 hover:bg-pink-200 transition duration-150 ease-out hover:ease-in hover:scale-110 m-4 p-3 w-36 rounded-lg shadow-lg">
+                  Create Account
+                </button>
+              </a>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
