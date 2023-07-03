3475fe924f9ee0fd3534789827ecd1e1e666ca44
justneil11 <justneil11@gmail.com>
2023-07-03 09:49:06 +0100

    added map

--- supplantree_react/src/components/Learn Page/LearnMap.js
+++ supplantree_react/src/components/Learn Page/LearnMap.js
@@ -3,7 +3,13 @@ import {MapContainer, TileLayer, Marker, Popup} from "react-leaflet"
 
 const LearnMap = () => {
     return ( 
-        <h1 className="" >LearnMap</h1>
+        <div>
+        <img
+            className="w-10/12 md:w-5/12 place-self-center m-2 p-4 rounded-3xl"
+            src="./images/Scotland_region_map.svg"
+            alt="placeholder"
+          ></img>
+        </div>
      );
 }
  
