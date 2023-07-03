286372e275d1546436ac9b996642608c2d02d454
Cooper-Higgins <jamie@supplantree.com>
2023-07-03 14:32:55 +0100

    more styling

--- supplantree_react/src/components/Learn Page/LearnMap.js
+++ supplantree_react/src/components/Learn Page/LearnMap.js
@@ -3,19 +3,19 @@ import {MapContainer, TileLayer, Marker, Popup} from "react-leaflet"
 
 const LearnMap = () => {
     return ( 
-        <div className="">
-        <div className="flex flex-col justify-center p-4">
-        <h1 className="text-6xl p-4">Learn</h1>
-        <h3 className="text-4xl">Click a region to learn more about its soil</h3>
-
-       <div className="flex justify-evenly items-center gap-10 m-12">
+        <div className='flex justify-evenly'>
+        <div className="flex flex-col justify-center gap-48 w-1/3">
+            <h3 className="text-5xl">Click a region to learn more about its soil</h3>
+            <p className='text-center border-4 border-white bg-slate-300 rounded-xl'>
+                sdfrevgewFCEWfsdfrevgewFCEWfsdfrevgewFCEWsdfrevgewFCEWfsdfrevgew sdfrevgewFCEWfsdfrevgewFCEWfsdfrevgewFCEWsdfrevgewFCEWfsdfrevgew sdfrevgewFCEWfsdfrevgewFCEWfsdfrevgewFCEWsdfrevgewFCEWfsdfrevgew sdfrevgewFCEWfsdfrevgewFCEWfsdfrevgewFCEWsdfrevgewFCEWfsdfrevgew sdfrevgewFCEWfsdfrevgewFCEWfsdfrevgewFCEWsdfrevgewFCEWfsdfrevgew
+                
+            </p>
+       </div>
             <img
-                className="place-self-center w-6/12 md:w-4/12 mb-4 md:mb-12"
+                className="w-6/12 md:w-4/12 mb-4 md:mb-12"
                 src="./images/Scotland_region_map.svg"
                 alt="interactive scotland map"
             ></img>
-            </div>
-      </div>
       </div>
      );
 }
