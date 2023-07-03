64da8cb5db97e908a26651bfde6c52250b439437
justneil11 <justneil11@gmail.com>
2023-07-03 09:59:41 +0100

    Revert "added map"
    This reverts commit 3475fe924f9ee0fd3534789827ecd1e1e666ca44.

--- supplantree_react/src/components/Learn Page/LearnMap.js
+++ supplantree_react/src/components/Learn Page/LearnMap.js
@@ -3,13 +3,7 @@ import {MapContainer, TileLayer, Marker, Popup} from "react-leaflet"
 
 const LearnMap = () => {
     return ( 
-        <div>
-        <img
-            className="w-10/12 md:w-5/12 place-self-center m-2 p-4 rounded-3xl"
-            src="./images/Scotland_region_map.svg"
-            alt="placeholder"
-          ></img>
-        </div>
+        <h1 className="" >LearnMap</h1>
      );
 }
  
