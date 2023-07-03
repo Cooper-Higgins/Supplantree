556e93aaa7cb78ab2ae9e7d91b7873a5014aaffe
Cooper-Higgins <jamie@supplantree.com>
2023-07-03 14:09:34 +0100

    started learn page

--- supplantree_react/src/components/Learn Page/LearnMap.js
+++ supplantree_react/src/components/Learn Page/LearnMap.js
@@ -3,7 +3,20 @@ import {MapContainer, TileLayer, Marker, Popup} from "react-leaflet"
 
 const LearnMap = () => {
     return ( 
-        <h1 className="" >LearnMap</h1>
+        <div className="">
+        <div className="flex flex-col justify-center p-4">
+        <h1 className="text-6xl p-4">Learn</h1>
+        <h3 className="text-4xl">Click a region to learn more about its soil</h3>
+
+       <div className="flex justify-evenly items-center gap-10 m-12">
+            <img
+                className="place-self-center w-6/12 md:w-4/12 mb-4 md:mb-12"
+                src="./images/Scotland_region_map.svg"
+                alt="interactive scotland map"
+            ></img>
+            </div>
+      </div>
+      </div>
      );
 }
  
--- supplantree_react/src/containers/LearnContainer.js
+++ supplantree_react/src/containers/LearnContainer.js
@@ -4,16 +4,15 @@ import LearnMap from "../components/Learn Page/LearnMap";
 const LearnContainer = () => {
   return (
     <div className="bg-green-400 min-h-screen">
-      <h1 className="text-6xl p-4">Learn</h1>
       <LearnMap />
-      <div className="flex justify-center my-20">
+      <div className="flex justify-center">
         <a href="http://localhost:3001/plant">
           <button className="ml-10 bg-slate-300 border-4 border-white hover:bg-pink-200 transition duration-150 ease-out hover:ease-in hover:scale-110 m-4 p-3 w-36 rounded-lg shadow-lg shadow-gray-600">
             Get Planting
           </button>
         </a>
       </div>
-    </div>
+      </div>
   );
 };
 
